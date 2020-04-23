3f09eb125399444af50d935b88a505a7d67c20fa
janfalih17 <bosspulsa57@gmail.com>
2020-04-23 19:43:22 +0700

    Membuat Variable Pada Golang

---
+++ 2__Variable/variable.go
@@ -0,0 +1,19 @@
+package main
+
+//Memanggil fmt dari package golang
+import (
+	"fmt"
+)
+
+//Membuat fungsi main yang akan di jalankan di golang
+func main() {
+	var firstname string = "Jan Falih"
+	tinggal := "Tangerang"
+
+	//Membuat printf
+	fmt.Printf("Ini Pake Printf (Saya %s Berasal Dari %s)\n", firstname, tinggal)
+	//Perbedaan Dengan Println
+	fmt.Println("Ini Pake Println (Saya %s Berasal Dari %s)\n", firstname, tinggal)
+	//Perbedaan Dengan Print
+	fmt.Print("Ini Pake Print (Saya %s Berasal Dari %s)\n", firstname, tinggal)
+}
